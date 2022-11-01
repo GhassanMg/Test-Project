17b1fddec0c78df8c0fb1d91cef4aad93d84e85a
ghassan_mg <ghassan.mgsyr@gmail.com>
2022-11-01 17:55:34 +0300

    create role controller

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -8,7 +8,6 @@ use App\Http\Requests\Api\User\UpdateProfileRequest;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
