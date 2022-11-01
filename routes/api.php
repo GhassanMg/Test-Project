a6edd85e733f77ba5eec2b71c736792002fdea3c
ghassan_mg <ghassan.mgsyr@gmail.com>
2022-11-01 16:31:03 +0300

    set middleware for routes

--- routes/api.php
+++ routes/api.php
@@ -16,27 +16,35 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+// Login and register API
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
 
-    // User Management
-    Route::get('profile', [UserController::class, 'get_current_user_profile']);
-    Route::post('profile/update', [UserController::class, 'update_user_profile']);
-    Route::post('password/update', [UserController::class, 'update_password']);
+    //Group Of Admin Api's
+    Route::group(['middleware' => ['role:admin']], function () {
 
-    //Roles Management
-    Route::get('user/roles', [UserController::class, 'get_user_roles']);
+        //Profile
+        Route::get('profile', [UserController::class, 'get_current_user_profile']);
 
-    //Product Management
-    Route::get('products', [ProductController::class, 'get_all_products']);
-    Route::get('user/products', [ProductController::class, 'get_user_products']);
-    Route::post('product/add', [ProductController::class, 'add_product']);
-    Route::post('product/update', [ProductController::class, 'edit_product']);
-    Route::post('product/delete', [ProductController::class, 'delete_product']);
-    Route::post('user/products/assign', [ProductController::class, 'assign_product_to_user']);
+        // User Management
+        Route::post('profile/update', [UserController::class, 'update_user_profile']);
+        Route::post('password/update', [UserController::class, 'update_password']);
 
+        //Roles Management
+        Route::get('user/roles', [UserController::class, 'get_user_roles']);
+
+        //Product Management
+        Route::get('products', [ProductController::class, 'get_all_products']);
+        Route::get('products/user', [ProductController::class, 'get_user_products_by_admin']);
+        Route::post('product/add', [ProductController::class, 'add_product']);
+        Route::post('product/update', [ProductController::class, 'edit_product']);
+        Route::post('product/delete', [ProductController::class, 'delete_product']);
+        Route::post('user/products/assign', [ProductController::class, 'assign_product_to_user']);
+    });
+
+    //Current User Products Admin & User
+    Route::get('user/products', [ProductController::class, 'get_user_products_by_user']);
 });
 
